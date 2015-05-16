4167bec0d07ec94b3bc6d716a7518f4aba081898
Ilya Manyahin <i.manyahin@gmail.com>
2015-05-16 18:15:48 +0300

    Make export values to mongodb

--- read.js
+++ read.js
@@ -6,10 +6,57 @@ var serialArduinoPort = new SerialPort("/dev/cu.usbmodemfd121", {
   parser: serialport.parsers.readline("\n")
 });
 
+var MongoClient = require('mongodb').MongoClient;
+var collection = null;
+
+MongoClient.connect("mongodb://localhost:27017/metrics", function(err, db) {
+  if (err) { return console.dir(err); }
+
+  console.log('Succesfulluy connected to MongoDB');
+
+  collection = db.collection('temperatures');
+});
+
 serialArduinoPort.on("open", function () {
-  console.log('Succesfulluy connectd to Arduino serial port');
+	console.log('Succesfulluy connectd to Arduino serial port');
 
   serialArduinoPort.on('data', function(data) {
-    console.log('Current Temperature: ' + data);
+  	var temperature = data.trim();
+
+    console.log('Received Temperature: ' + temperature);
+
+    // Current second. 
+    var second = new Date().getSeconds();
+
+    // Generate rule for update.
+    var key_name_second = "values." + second; 
+    var updated_value = {};
+    updated_value[key_name_second] = temperature;
+
+    // Update/Insert new value to database.
+    collection.update({
+  		timestamp : getTimestamp()
+  	}, {
+  		$set : updated_value
+  	}, {
+  		upsert : true
+  	});
+
   });
+
 });
+
+// Function return current date except of seconds. 
+function getTimestamp() {
+	var currentDate = new Date();
+
+	var generatedDate = new Date(
+		currentDate.getFullYear(), 
+		currentDate.getMonth(),
+		currentDate.getDate(),
+		currentDate.getHours() + 3, // Fix time zone.
+		currentDate.getMinutes()
+	);
+
+	return generatedDate.toISOString();
+}
